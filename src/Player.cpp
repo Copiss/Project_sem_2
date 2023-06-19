939503ef8f0ef28e94226469e6c776189a0769b6
Antonallo <100342766+Antonallo@users.noreply.github.com>
2023-06-19 20:28:55 +0200

    asa

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,22 +4,38 @@ namespace game
 {
     PlayerCircle::PlayerCircle() : Circle(30.f, sf::Color::Green) {}
 
+    std::unique_ptr<PlayerCircle> PlayerCircle::instance = nullptr;
+
+    PlayerCircle& PlayerCircle::getInstance()
+    {
+        if (instance == nullptr)
+        {
+            instance.reset(new PlayerCircle());
+        }
+        return *instance;
+    }
+
+    void PlayerCircle::update()
+    {
+        handleInput();
+    }
+
     void PlayerCircle::move(float x, float y) {
         m_shape.move(x, y);
     }
 
     void PlayerCircle::handleInput() {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            move(-0.1f, 0.f);
+            move(-4.f, 0.f);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            move(0.1f, 0.f);
+            move(4.f, 0.f);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            move(0.f, -0.1f);
+            move(0.f, -4.f);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            move(0.f, 0.1f);
+            move(0.f, 4.f);
         }
     }
 }
